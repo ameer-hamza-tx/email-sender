d568e0b92c55f51f5c825d5e64b0cd71dbec162a
hamza <ameer.hamza@techcreatix.com>
2021-02-25 16:51:51 +0500

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,13 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.smtp_settings = {
+    :user_name => "ameerhamza112281@gmail.com",
+    :password => "03226518570",
+    :domain => 'gmail.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
